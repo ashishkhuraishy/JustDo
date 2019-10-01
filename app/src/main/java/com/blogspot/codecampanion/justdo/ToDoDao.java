8042a72f659a8d36a207871024c606d0d4fd4a91
ashishkhuraishy <42002670+ashishkhuraishy@users.noreply.github.com>
2019-10-01 12:33:31 +0530

    Created Dao

---
+++ app/src/main/java/com/blogspot/codecampanion/justdo/ToDoDao.java
@@ -0,0 +1,31 @@
+package com.blogspot.codecampanion.justdo;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface ToDoDao {
+
+    @Insert
+    void insert(ToDo toDo);
+
+    @Update
+    void update(ToDo toDo);
+
+    @Delete
+    void delete(ToDo toDo);
+
+    @Query("DELETE FROM todo_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM todo_table")
+    LiveData<List<ToDo>> getAllTasks();
+
+
+}
