b7985b6fb018882695246ee5c77d60c36f4243fd
ashishkhuraishy <42002670+ashishkhuraishy@users.noreply.github.com>
2019-10-01 20:38:42 +0530

    Set Up RecyclerView

--- app/src/main/java/com/blogspot/codecampanion/justdo/MainActivity.java
+++ app/src/main/java/com/blogspot/codecampanion/justdo/MainActivity.java
@@ -2,15 +2,15 @@ package com.blogspot.codecampanion.justdo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private ToDoViewModel viewModel;
 
@@ -19,11 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final ToDoAdapter adapter = new ToDoAdapter();
+        recyclerView.setAdapter(adapter);
+
         viewModel = ViewModelProviders.of(this).get(ToDoViewModel.class);
         viewModel.getData().observe(this, new Observer<List<ToDo>>() {
             @Override
             public void onChanged(List<ToDo> toDos) {
                 //Update Recycler View
+                adapter.setTasks(toDos);
             }
         });
 
---
+++ app/src/main/java/com/blogspot/codecampanion/justdo/ToDoAdapter.java
@@ -0,0 +1,58 @@
+package com.blogspot.codecampanion.justdo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder> {
+
+    private List<ToDo> tasks;
+
+    public void setTasks(List<ToDo> tasks) {
+        this.tasks = tasks;
+        notifyDataSetChanged();
+    }
+
+    class ToDoViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView taskText;
+        private TextView subTaskText;
+
+        ToDoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            taskText = itemView.findViewById(R.id.taskText);
+            subTaskText = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ToDoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.list_activity, parent, false
+        );
+
+        return new ToDoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ToDoViewHolder holder, int position) {
+        ToDo currentItem = tasks.get(position);
+
+        holder.taskText.setText(currentItem.getTask());
+        holder.subTaskText.setText(currentItem.getSubtask());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+
+}
