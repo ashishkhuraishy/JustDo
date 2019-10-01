0a6c87c8f7bc7baf0199ac231a3190a120a56299
ashishkhuraishy <42002670+ashishkhuraishy@users.noreply.github.com>
2019-10-01 12:47:20 +0530

    Created RoomDatabase

---
+++ app/src/main/java/com/blogspot/codecampanion/justdo/ToDoDataBase.java
@@ -0,0 +1,26 @@
+package com.blogspot.codecampanion.justdo;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = ToDo.class, version = 1)
+public abstract class ToDoDataBase extends RoomDatabase {
+
+    private static ToDoDataBase instance;
+
+    public abstract ToDoDao toDoDao();
+
+    private static synchronized ToDoDataBase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    ToDoDataBase.class, "todo_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+
+}
