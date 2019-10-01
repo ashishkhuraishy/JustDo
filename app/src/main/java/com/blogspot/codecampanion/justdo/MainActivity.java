1e3367f144423d400986ec5e0923d4534a92d60f
ashishkhuraishy <42002670+ashishkhuraishy@users.noreply.github.com>
2019-10-01 13:35:53 +0530

    Editing MainActivity

--- app/src/main/java/com/blogspot/codecampanion/justdo/MainActivity.java
+++ app/src/main/java/com/blogspot/codecampanion/justdo/MainActivity.java
@@ -1,14 +1,31 @@
 package com.blogspot.codecampanion.justdo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private ToDoViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewModel = ViewModelProviders.of(this).get(ToDoViewModel.class);
+        viewModel.getData().observe(this, new Observer<List<ToDo>>() {
+            @Override
+            public void onChanged(List<ToDo> toDos) {
+                //Update Recycler View
+            }
+        });
+
     }
 }
